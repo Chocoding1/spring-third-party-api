d2fb2ab57e1f40afdf5e0b112abc412dca305617
Chocoding1 <galmeagi2@naver.com>
2026-06-16 12:51:05 +0900

    feat: 주문 금액 검증 로직 구현

--- learning-test-1-api-error/initial/src/main/java/woowacourse/payment/order/Order.java
+++ learning-test-1-api-error/initial/src/main/java/woowacourse/payment/order/Order.java
@@ -1,24 +1,31 @@
 package woowacourse.payment.order;
 
+import woowacourse.payment.PaymentAmountMismatchException;
+
 /**
  * 결제 전에 서버가 미리 저장해 두는 주문 정보. successUrl 의 amount 와 대조해 금액 위변조를 막는 기준값이다.
  */
 public class Order {
 
-  private final String orderId;
-  private final Long amount;
+    private final String orderId;
+    private final Long amount;
 
-  public Order(String orderId, Long amount) {
-    this.orderId = orderId;
-    this.amount = amount;
-  }
+    public Order(String orderId, Long amount) {
+        this.orderId = orderId;
+        this.amount = amount;
+    }
 
-  public String getOrderId() {
-    return orderId;
-  }
+    public String getOrderId() {
+        return orderId;
+    }
 
-  public Long getAmount() {
-    return amount;
-  }
+    public Long getAmount() {
+        return amount;
+    }
 
+    public void validateAmount(Long amount) {
+        if (!this.amount.equals(amount)) {
+            throw new PaymentAmountMismatchException(this.amount, amount);
+        }
+    }
 }
